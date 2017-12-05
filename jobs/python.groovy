cba0f07d660f1a8787ffc6cd535a9ef5d1c4e3a9
root <root@ip-172-31-45-238.eu-west-1.compute.internal>
2017-12-05 17:34:32 +0000

    nuevo groovy

---
+++ jobs/python.groovy
@@ -0,0 +1,25 @@
+project_name = "jenkins-pipeline-python"
+repo = "https://github.com/suaveyosi/test-jenkins-1.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+    definition {
+	triggers{
+	   scm('* * * * *')
+        }
+
+	cpsScm{
+	    scm {
+		git {
+		    remote {
+		       name(repo_name)
+                       url(repo)
+                    }
+
+		}
+		scriptPath("Jenkinsfile")
+	    }
+	}
+
+    }
+}
